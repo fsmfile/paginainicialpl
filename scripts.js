acc1ec286af162f205afb1fc3391c434140be098
fsmfile <121582569+fsmfile@users.noreply.github.com>
2024-07-30 13:26:25 -0300

    corrigidos os tons das cores de acordo com o padrão da CS

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const menuItems = [
     { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
-    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
-    { text: 'Painel dos Casos de Referência', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
-    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' }
+    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiOGE0ZjVmNDUtNTE0Zi00YTQ0LTlkYzktZWExNTI0Mjc5YmFiIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
+    { text: 'Painel dos Casos de Referência', link: 'https://sintegre.ons.org.br/sites/8/casosreferencia' },
+    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYWFlNmQ0NWItMTRmYi00MDQzLWEwMjUtNDYxMGQ1MjM5MTY2IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' }
 ];
 
 function toggleMenu() {
