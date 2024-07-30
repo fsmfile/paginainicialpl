fa07103f435dc98cf50d68d2c0c4a1c8d973e9f0
fsmfile <121582569+fsmfile@users.noreply.github.com>
2024-07-30 12:48:19 -0300

    Tudo funcionando para abrir os links com o menu ativo e a barra superior.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const menuItems = [
     { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
-    { text: 'Reuniões GTs estados', link: '#' },
-    { text: 'Painel dos Casos de Referência', link: '#' },
-    { text: 'Margens para os Leilões de Energia', link: '#' }
+    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
+    { text: 'Painel dos Casos de Referência', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
+    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' }
 ];
 
 function toggleMenu() {
@@ -18,9 +18,29 @@ function generateMenu() {
         const a = document.createElement('a');
         a.href = item.link;
         a.textContent = item.text;
+        a.addEventListener('click', function(event) {
+            event.preventDefault();
+            loadContent(item.link);
+        });
         li.appendChild(a);
         menuList.appendChild(li);
     });
 }
 
-document.addEventListener('DOMContentLoaded', generateMenu);
+function loadContent(url) {
+    const contentFrame = document.getElementById('content-frame');
+    contentFrame.src = url; // Carrega o novo conteúdo no iframe
+}
+
+function closeMenu(event) {
+    const sidebar = document.getElementById('sidebar');
+    const menuIcon = document.getElementById('menu-icon');
+    if (!sidebar.contains(event.target) && !menuIcon.contains(event.target)) {
+        sidebar.classList.remove('open');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    generateMenu();
+    document.addEventListener('click', closeMenu);
+});
