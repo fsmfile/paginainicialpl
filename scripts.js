8a0697ebbf4dcf77c173a82c06db251c4fc19b65
fsmfile <121582569+fsmfile@users.noreply.github.com>
2024-09-16 12:31:03 -0300

    primeiro commit online

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const menuItems = [
-    { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' },
-    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/reportEmbed?reportId=51b5ceaf-64ea-4549-b175-c4fdd91663f0&autoAuth=true&ctid=3adee5fc-933e-4911-ae1b-9c2fe7b84448' },
-    { text: 'Painel dos Casos de Referência', link: 'https://sintegre.ons.org.br/sites/8/casosreferencia' },
-    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYWFlNmQ0NWItMTRmYi00MDQzLWEwMjUtNDYxMGQ1MjM5MTY2IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9' }
+    { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Sumário Executivo e sua apresentação, relatórios e Infográfico.' },
+    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiOGE0ZjVmNDUtNTE0Zi00YTQ0LTlkYzktZWExNTI0Mjc5YmFiIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Gestão dos planos de ações, apresentações, atas de reuniões e Infográfico.' },
+    { text: 'Painel dos Casos de Referência', link: 'https://app.powerbi.com/view?r=eyJrIjoiZTFmMWFlMjEtZGQ2NS00MDhkLTkzYTktNjJhNTBiNTkyZjk1IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Dashboard dos casos dos estudos de Curto-Prazo: PAR/PEL, Mensal e Quadrimestral.' },
+    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYWFlNmQ0NWItMTRmYi00MDQzLWEwMjUtNDYxMGQ1MjM5MTY2IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para leilões de energia.' },
+    { text: 'Margem por Barramento', link: 'https://app.powerbi.com/view?r=eyJrIjoiNjkxMWQxMmYtNDlhZS00MzNmLTgzODctMjBhZGM2YTRjODNlIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para escoamento de geração.' },
 ];
+
 
 function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
@@ -18,15 +20,17 @@ function generateMenu() {
         const a = document.createElement('a');
         a.href = item.link;
         a.textContent = item.text;
+        a.title = item.altText; // Adiciona o texto alternativo como tooltip
         a.addEventListener('click', function(event) {
             event.preventDefault();
             loadContent(item.link);
-            closeMenu(); // Fecha o menu ao clicar em um item
+            closeMenu();
         });
         li.appendChild(a);
         menuList.appendChild(li);
     });
 }
+
 
 function loadContent(url) {
     const contentFrame = document.getElementById('content-frame');
@@ -47,8 +51,10 @@ document.addEventListener('DOMContentLoaded', function() {
             closeMenu();
         }
     });
+
+    // Adiciona evento de clique no logo para voltar à página inicial
+    document.getElementById('logo-home').addEventListener('click', function() {
+        loadContent('home.html'); // Carrega a página inicial no iframe
+    });
 });
 
-document.getElementById('menu-icon').addEventListener('click', function(event) {
-    event.stopPropagation();
-});
