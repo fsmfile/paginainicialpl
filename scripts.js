4e253b4c8876493e3e68f1b88c602faddcaa8f4e
fsmfile <121582569+fsmfile@users.noreply.github.com>
2024-09-16 12:39:17 -0300

    script alterado para realizar leitura dos itens do menu no json

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
-const menuItems = [
-    { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Sumário Executivo e sua apresentação, relatórios e Infográfico.' },
-    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiOGE0ZjVmNDUtNTE0Zi00YTQ0LTlkYzktZWExNTI0Mjc5YmFiIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Gestão dos planos de ações, apresentações, atas de reuniões e Infográfico.' },
-    { text: 'Painel dos Casos de Referência', link: 'https://app.powerbi.com/view?r=eyJrIjoiZTFmMWFlMjEtZGQ2NS00MDhkLTkzYTktNjJhNTBiNTkyZjk1IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Dashboard dos casos dos estudos de Curto-Prazo: PAR/PEL, Mensal e Quadrimestral.' },
-    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYWFlNmQ0NWItMTRmYi00MDQzLWEwMjUtNDYxMGQ1MjM5MTY2IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para leilões de energia.' },
-    { text: 'Margem por Barramento', link: 'https://app.powerbi.com/view?r=eyJrIjoiNjkxMWQxMmYtNDlhZS00MzNmLTgzODctMjBhZGM2YTRjODNlIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para escoamento de geração.' },
-];
+//const menuItems = [
+//    { text: 'Estudos PAR/PEL', link: 'https://app.powerbi.com/view?r=eyJrIjoiYjE4YjM4ZjMtNDg3MS00NTNhLTgxN2UtZDljZmI5YmQ1MTljIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Sumário Executivo e sua apresentação, relatórios e Infográfico.' },
+//    { text: 'Reuniões GTs estados', link: 'https://app.powerbi.com/view?r=eyJrIjoiOGE0ZjVmNDUtNTE0Zi00YTQ0LTlkYzktZWExNTI0Mjc5YmFiIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Gestão dos planos de ações, apresentações, atas de reuniões e Infográfico.' },
+//    { text: 'Painel dos Casos de Referência', link: 'https://app.powerbi.com/view?r=eyJrIjoiZTFmMWFlMjEtZGQ2NS00MDhkLTkzYTktNjJhNTBiNTkyZjk1IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Dashboard dos casos dos estudos de Curto-Prazo: PAR/PEL, Mensal e Quadrimestral.' },
+//    { text: 'Margens para os Leilões de Energia', link: 'https://app.powerbi.com/view?r=eyJrIjoiYWFlNmQ0NWItMTRmYi00MDQzLWEwMjUtNDYxMGQ1MjM5MTY2IiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para leilões de energia.' },
+//    { text: 'Margem por Barramento', link: 'https://app.powerbi.com/view?r=eyJrIjoiNjkxMWQxMmYtNDlhZS00MzNmLTgzODctMjBhZGM2YTRjODNlIiwidCI6IjNhZGVlNWZjLTkzM2UtNDkxMS1hZTFiLTljMmZlN2I4NDQ0OCIsImMiOjR9', altText: 'Capacidade remanescente do SIN para escoamento de geração.' },
+//];
 
 
 function toggleMenu() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('open');
+}
+
+
+
+let menuItems = [];
+
+function fetchMenuItems() {
+    fetch('https://raw.githubusercontent.com/fsmfile/paginainicialpl/master/menuItems.json')
+        .then(response => response.json())
+        .then(data => {
+            menuItems = data;
+            generateMenu(); // Gera o menu após carregar os dados
+        })
+        .catch(error => console.error('Erro ao carregar o menu:', error));
 }
 
 function generateMenu() {
@@ -20,7 +34,7 @@ function generateMenu() {
         const a = document.createElement('a');
         a.href = item.link;
         a.textContent = item.text;
-        a.title = item.altText; // Adiciona o texto alternativo como tooltip
+        a.title = item.altText;
         a.addEventListener('click', function(event) {
             event.preventDefault();
             loadContent(item.link);
@@ -30,6 +44,44 @@ function generateMenu() {
         menuList.appendChild(li);
     });
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchMenuItems(); // Chama a função para buscar os dados
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//function generateMenu() {
+//    const menuList = document.getElementById('menu-items');
+ //   menuList.innerHTML = '';
+//    menuItems.forEach(item => {
+ //       const li = document.createElement('li');
+ //       const a = document.createElement('a');
+  //      a.href = item.link;
+ //       a.textContent = item.text;
+ //       a.title = item.altText; // Adiciona o texto alternativo como tooltip
+ //       a.addEventListener('click', function(event) {
+//            event.preventDefault();
+ //           loadContent(item.link);
+//            closeMenu();
+//        });
+//        li.appendChild(a);
+//        menuList.appendChild(li);
+//    });
+//}
 
 
 function loadContent(url) {
